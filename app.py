7de478522b852631316e3e82c64ab79dbe4d3c84
AsepGinanjar <asepginanajr2004@gmail.com>
2025-04-28 00:24:40 +0700

    update ci 2

--- app.py
+++ app.py
@@ -705,7 +705,7 @@ def home():
                     </div>
                     
                     <div class="footer-bottom">
-                        <p>&copy; {{ current_year }} AAAAA | Dibuat oleh <span class="developer">{{ developer_name }} ({{ developer_nrp }})</span></p>
+                        <p>&copy; {{ current_year }} AAAAASPEPPPP | Dibuat oleh <span class="developer">{{ developer_name }} ({{ developer_nrp }})</span></p>
                     </div>
                 </div>
             </footer>
